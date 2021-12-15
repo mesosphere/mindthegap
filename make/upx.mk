3f3eae39555df1f0b0e48b1c4486c1c36ba8e497
Jimmi Dyson <jimmidyson@gmail.com>
2021-12-15 13:10:31 +0000

    build: Skip UPX on snapshot builds (#7)

--- make/upx.mk
+++ make/upx.mk
@@ -14,6 +14,7 @@
 
 .PHONY: upx
 upx: UPX_REAL_TARGET := $(addsuffix $(if $(filter $(GOOS),windows),.exe),$(basename $(UPX_TARGET)))
+ifneq ($(IS_SNAPSHOT),true)
 ifeq ($(GOOS)/$(GOARCH),windows/arm64)
 upx: ; $(info $(M) skipping packing $(UPX_REAL_TARGET) - $(GOOS)/$(GOARCH) is not yet supported by upx)
 else
@@ -21,4 +22,5 @@ upx: install-tool.upx
 upx: ## Pack executable using upx
 upx: ; $(info $(M) packing $(UPX_REAL_TARGET))
 	(upx -l $(UPX_REAL_TARGET) &>/dev/null && echo $(UPX_REAL_TARGET) is already packed) || upx -9 $(UPX_REAL_TARGET)
+endif
 endif
