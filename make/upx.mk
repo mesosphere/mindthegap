503d72631ef1220244c578c518bdb3110026eaa6
Jimmi Dyson <jimmidyson@gmail.com>
2022-03-18 13:02:09 +0000

    fix: Do not upx pack darwin binaries
    upx sometimes produces corrupt binaries for darwin.

--- make/upx.mk
+++ make/upx.mk
@@ -6,10 +6,14 @@ upx: UPX_REAL_TARGET := $(addsuffix $(if $(filter $(GOOS),windows),.exe),$(basen
 ifneq ($(IS_SNAPSHOT),true)
 ifeq ($(GOOS)/$(GOARCH),windows/arm64)
 upx: ; $(info $(M) skipping packing $(UPX_REAL_TARGET) - $(GOOS)/$(GOARCH) is not yet supported by upx)
+else ifeq ($(GOOS),darwin)
+upx: ; $(info $(M) skipping packing $(UPX_REAL_TARGET) - upx produces corrupt binaries for $(GOOS))
 else
 upx: install-tool.upx
 upx: ## Pack executable using upx
 upx: ; $(info $(M) packing $(UPX_REAL_TARGET))
 	(upx -l $(UPX_REAL_TARGET) &>/dev/null && echo $(UPX_REAL_TARGET) is already packed) || upx -9 $(UPX_REAL_TARGET)
+# Double check file is successfully compressed - seen errors with macos binaries
+	upx -t $(UPX_REAL_TARGET) &>/dev/null || (echo $(UPX_REAL_TARGET) is broken after upx compression && exit 1)
 endif
 endif
