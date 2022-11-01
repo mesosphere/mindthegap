7ba9e33f6fef281ed178377558dd0f4d5d30c568
Jimmi Dyson <jimmidyson@gmail.com>
2022-11-01 15:42:21 +0000

    build: Add magefile and pre-commit target

---
+++ magefiles/lint.go
@@ -0,0 +1,31 @@
+// Copyright 2021 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build mage
+
+package main
+
+import (
+	"context"
+
+	_ "github.com/magefile/mage/mage"
+	"github.com/magefile/mage/mg"
+	"github.com/mesosphere/daggers/dagger/options"
+	precommitdagger "github.com/mesosphere/daggers/dagger/precommit"
+	"github.com/mesosphere/daggers/mage/precommit"
+)
+
+type Lint mg.Namespace
+
+func (Lint) Precommit(ctx context.Context) error {
+	return precommit.PrecommitWithOptions(ctx,
+		precommitdagger.CustomizeContainer(
+			options.DownloadExecutableFile(
+				ctx,
+				"https://github.com/mvdan/sh/releases/download/v3.5.1/shfmt_v3.5.1_linux_amd64",
+				"/usr/local/bin/shfmt",
+			),
+			options.InstallGo(ctx),
+		),
+	)
+}
---
+++ magefiles/mage.go
@@ -0,0 +1,16 @@
+// Copyright 2021 D2iQ, Inc. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build ignore
+
+package main
+
+import (
+	"os"
+
+	"github.com/magefile/mage/mage"
+)
+
+func main() {
+	os.Exit(mage.Main())
+}
